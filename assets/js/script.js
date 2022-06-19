5127bac828ea26d4973ba2a25a17bb2d7d320c88
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-06-19 18:24:08 -0500

    pressing the button does start the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// TASKS FOR THE CODE
+// 1) Press Start to start the quiz
+//      a)Hide the start page
+// 2) Start the timer
+//      a)use this as main function to start Time
+//      b)load question
+//      c) keep score
+//      d) enter high score
+// 3) Ask a question
+// 4) Event listener for right answer
+// 5) If get it wrong, subract time
+// 6) Input high score
+
+// Press start to start quiz
+var buttonEl = document.querySelector("#startQuiz");
+console.log(buttonEl);
+// This gets the quiz started
+buttonEl.addEventListener("click", function() {
+    console.log("This will start the quiz!");
+    
+});
