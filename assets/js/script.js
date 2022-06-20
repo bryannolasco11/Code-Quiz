f59b3d7978332d3084cc17f580a27c56a692738e
Bryan Nolasco <bryan_nolasco@hotmail.com>
2022-06-19 23:47:50 -0500

    I have added a generic question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,8 +20,44 @@ buttonEl.addEventListener("click", function() {
     console.log("This will start the quiz!");
     // I will make the instructions invisible
     introduction.setAttribute("style", "display:none;");
-    }
-
-        
     
-);
+    //qcontainer.setAttribute("style", "display:visible");
+    quizQuestion();
+});
+    
+
+// Creates a question
+
+function quizQuestion() {
+    var body = document.body;
+    var divEl = document.createElement("div");
+    var listEl= document.createElement("ol");
+    var li1= document.createElement("li");
+    var li2= document.createElement("li");
+    var li3= document.createElement("li");
+    var li4= document.createElement("li");
+
+    listEl.textContent = "What color is the sky?";
+    li1.textContent = "Red";
+    li2.textContent = "Orange";
+    li3.textContent = "Blue";
+    li4.textContent = "Green";
+
+    body.appendChild(divEl);
+    divEl.appendChild(listEl);
+    listEl.appendChild(li1);
+    listEl.appendChild(li2);
+    listEl.appendChild(li3);
+    listEl.appendChild(li4);
+
+
+    //questionEl.className = "question";
+
+    //var questionContainer = document.getElementById("qcontainer");
+    // give it a class name
+
+    //questionEl.appendChild(questionContainer);
+    //questionEl.textContent="question";
+    console.log("This is a new div");
+    
+};
